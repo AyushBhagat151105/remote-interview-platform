ea9baaa61d9e312397483ecac73f3d0b0366ad55
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-07-31 17:57:07 +0530

    feat: add comments and interviews functionality with corresponding API and schema updates

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const syncUser = mutation({
   args: {
@@ -10,16 +10,49 @@ export const syncUser = mutation({
   },
 
   handler: async (ctx, args) => {
-    const existingUser = await ctx.db
+    const email = args.email.trim().toLowerCase();
+
+    const existingUserByClerkId = await ctx.db
       .query("users")
-      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
       .first();
 
-    if (existingUser) return;
+    const existingUserByEmail = await ctx.db
+      .query("users")
+      .withIndex("by_email", (q) => q.eq("email", email))
+      .first();
+
+    if (existingUserByClerkId || existingUserByEmail) return;
 
     return await ctx.db.insert("users", {
-      ...args,
+      name: args.name,
+      email,
+      clerkId: args.clerkId,
+      image: args.image,
       role: "candidate",
     });
+  },
+});
+
+export const getUsers = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("User is not authenticated");
+
+    const users = await ctx.db.query("users").collect();
+
+    return users;
+  },
+});
+
+export const getUserByClerkId = query({
+  args: { clerkId: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
+      .first();
+
+    return user;
   },
 });
